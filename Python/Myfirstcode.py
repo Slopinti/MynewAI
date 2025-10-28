5d0eb417a35ef3b6018b8edbf94ca25d18f15ccc
Suresh Lopinti <sureshkl11234@gmail.com>
2025-10-28 19:02:25 -0400

    my first commit

---
+++ Python/Myfirstcode.py
@@ -0,0 +1,7 @@
+print("hai suresh kumar ")
+print("welcome to python programming")
+print("this is my first code in python")
+print("i am learning python from youtube")
+
+
+print("python is easy to learn and fun to use")
